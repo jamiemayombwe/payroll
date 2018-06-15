3cc97b1fef73e26f61cc14a4d2712d9930afe812
jamillah <jamillah@laboremus.no>
2018-06-15 17:25:10 +0300

    Adds static root to settings

--- payroll/settings.py
+++ payroll/settings.py
@@ -134,6 +134,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR + "/static"),
 )
 
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
