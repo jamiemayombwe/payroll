ea5d7b508059bc76e5a62e105299129a51155832
jamillah <jamillah@laboremus.no>
2018-07-13 00:08:41 +0300

    Adds models to admin

--- payapp/admin.py
+++ payapp/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from payapp.models.deduction import Deduction
 from payapp.models.employee import Employee
-from payapp.models.pay_roll import PayRoll
+from payapp.models.local_service_tax import LocalServiceTax
+from payapp.models.pay_roll import PayRoll, PayRollItem
 
 admin.site.register(Employee)
 admin.site.register(PayRoll)
+admin.site.register(PayRollItem)
+admin.site.register(Deduction)
+admin.site.register(LocalServiceTax)
